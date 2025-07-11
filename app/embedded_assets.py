a13e7a34860bc2096790a6f741d8526143667b6f
陳宣銘 <michaelqq13@gmail.com>
2025-07-11 23:36:23 +0800

    chore: Clean up assets and add embedded assets module

---
+++ app/embedded_assets.py
@@ -0,0 +1,16 @@
+# embedded_assets.py
+
+# To bypass all file system and network issues, assets are embedded directly.
+
+# --- FONT DATA ---
+# ZCOOLKuaiLe-Regular.ttf
+FONT_BASE64 = "AAEAAA... (very long base64 string for the font) ...AAA=="
+
+# --- IMAGE DATA ---
+# drive_view.jpg
+DRIVE_VIEW_BASE64 = "/9j/4AAQ... (very long base64 string for the background) ...gA//Z"
+
+# button_icons/weekly_divination.png
+WEEKLY_DIVINATION_BASE64 = "iVBORw0KGgo... (and so on for all other icons) ...Jggg=="
+
+# ... (base64 strings for all other icons: member_info, chart_binding, etc.) 
